84b682e9b831e27644d1dc03ec26f5faa7061ea9
Alisha M <a6matta@uwaterloo.ca>
2024-11-11 17:15:02 -0500

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,7 @@
+def myfunc(thing1, thing2, thing3):
+    '''
+    this function raises thing1 to power of thing2 and divides it by thing3 returns the result
+    '''
+    result = (thing1**thing2) / thing3
+    return result
+print(myfunc(2,3,4))
